7970f135aaab97a7ce65b4a441663f33bdaa5cbc
Vijay Dhyani <60819264+dhyanivj@users.noreply.github.com>
2021-01-04 14:15:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+import Header from "./header";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the linkedin clone</h1>
+      {/* header */}
+      <Header />
+      {/* App body */}
+      {/* sidebar */}
+      {/* feed */}
+      {/* widget */}
+    </div>
+  );
+}
