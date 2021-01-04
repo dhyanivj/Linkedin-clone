8398313f7c6b317f01e6f55b8b5ed7ebf38da137
Vijay Dhyani <60819264+dhyanivj@users.noreply.github.com>
2021-01-04 14:49:17 +0530

    header

--- src/header.js
+++ src/header.js
@@ -1,9 +1,20 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import "./header.css";
 
 function Header() {
   return (
     <div className="Header">
       <h1>This is header </h1>
+
+      <div className="header-left">
+        <img src="https://cdn3.iconfinder.com/data/icons/material-design-social-icons/152/Linkedin_icon-512.png" />
+        <div className="Header-search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header-right"></div>
     </div>
   );
 }
